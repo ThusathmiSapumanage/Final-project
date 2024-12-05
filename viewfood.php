87a0a81781ad823c7fe0a8ef0384835d12635760
ThusathmiSapumanage <thusathmivs@gmail.com>
2024-12-05 21:32:38 +0530

    Need to make nav bar in feedback

---
+++ viewfood.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> View Food and Beverage Suppliers </title>
+        <link rel="stylesheet" type="text/css" href="viewfood.css">
+    </head>
+    <body>
+        <div class="container">
+
+            <!-- Sidebar -->
+            <aside class = "sidebar">
+                <div class="logo">
+                    <img src="images/logo.png" alt="Logo">
+                </div>
+                <nav class = "menu">
+                    <a href="##"><img src = "">Events</a>
+                    <a href="supplierM.html" class = "active">Supplies</a>
+                    <a href="##">Finance</a>
+                    <a href="##">Staff</a>
+                    <a href="##">Resource</a>
+                    <a href="##">Client</a>
+                    <a href="feedback.html">Feedback</a>
+                </nav>
+                <hr class="section-divider"> 
+                <div class = "settings"><img src = Images/settings.png>Settings</div>
+            </aside>
+              <!-- Main Content -->
+            <main class = "content">
+                <header class="header">
+                    <h1>Supplier Management</h1>
+                    <div class="search">
+                        <input type="text" placeholder="Search">
+                        <img src="Images/search-interface-symbol.png">
+                        <button>Search</button>
+                    </div>
+            	</header>
+
+                <!-- Suppliers Section -->
+                <section class = "suppliers">
+                    <h2>Suppliers</h2>
+                    <div class = "list">
+                        <div class = "sup-card"><img src = "Images/user.png">PVT details</div>
+                        <div class = "sup-card"><img src = "Images/user.png">Sam and James details</div>
+                        <div class = "sup-card-add"><img src = "Images/add.png">Add supplier</div>
+                    </div>
+                </section>
